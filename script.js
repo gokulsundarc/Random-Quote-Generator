173784c3894ba9c37fec421d5356508ee3cb6dff
Gokulx07 <gokulsundar.x07@gmail.com>
2025-08-01 22:36:55 +0530

    Add files via upload
    Created a responsive web app using HTML, CSS, and JavaScript to display random motivational quotes. Implemented dynamic DOM manipulation with user interaction and modern styling using CSS Flexbox and gradients.

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const quotes = [
+  "The best way to get started is to quit talking and begin doing. – Walt Disney",
+  "Don’t let yesterday take up too much of today. – Will Rogers",
+  "It’s not whether you get knocked down, it’s whether you get up. – Vince Lombardi",
+  "Success is not in what you have, but who you are. – Bo Bennett",
+  "Your time is limited, so don’t waste it living someone else’s life. – Steve Jobs",
+  "nothing ever goes as planned"
+];
+
+function generateQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  document.getElementById("quote").textContent = quotes[randomIndex];
+}
